491a0fefb2bc60e8370faf12cfafcee0642fbdb8
Ameen-vv <mohammedameenvv@gmail.com>
2023-08-05 14:44:24 +0530

    first commit and done structuring folder for the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'user', pathMatch: 'full' },
+    {
+      path:'user',
+      loadChildren:() => import('./modules/user/user.module').then(m=>m.UserModule)
+    },
+    {
+      path:'doctor',
+      loadChildren:() => import('./modules/doctor/doctor.module').then(m=>m.DoctorModule)
+    },
+    {
+      path:'admin',
+      loadChildren:() => import('./modules/admin/admin.module').then(m=>m.AdminModule)
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
